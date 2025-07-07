ecf4950b3948c84af3edc2a4e3b731ef691c1d2d
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2025-07-07 12:25:58 +0300

    Add files via upload

---
+++ cookies.js
@@ -0,0 +1,6 @@
+//cookies are small pieces of data stored in a user's browser
+  //set a cookie
+document.cookie = "username = Fidel";
+//Retrieve a cookie value
+let username = 
+document.cookies.replace();
