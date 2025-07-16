6adb06c3f54ac3f5b9db7dd033bdc1fc3d65b467
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2025-07-16 10:54:37 +0300

    Add files via upload

---
+++ Encapsulation.js
@@ -0,0 +1,11 @@
+class BankAccount{
+    constructor(balance){
+        let_balance = balance;
+        this.getBalance = function(){
+            return_balance;
+        };
+    }
+}
+const account = new
+BankAccount(1000);
+console.log(account.getBalance());
