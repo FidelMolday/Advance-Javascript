699b754efc4c9635cd92ddb854d7f6759df209f1
Molday Developer <126654078+FidelMolday@users.noreply.github.com>
2025-07-16 10:53:37 +0300

    Add files via upload

---
+++ Polymorphism.js
@@ -0,0 +1,15 @@
+class Shape{
+    getArea(){
+        //calculate and return the area
+    }
+}
+class Circle extends Shape{
+    getArea(){
+        //calculate and return the area of a circle
+    }
+}
+class Rectangle extends Shape{
+    getArea(){
+        //calculate and return the area of a rectangle.
+    }
+}
